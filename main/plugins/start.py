cc30cff6092fdb5f93f81397f29db96fa0a318ff
Sharfatali <103334375+Sharfatali@users.noreply.github.com>
2022-04-18 00:17:02 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -49,8 +49,8 @@ async def notice(event):
 async def source(event):
     await event.edit(source_text,
                     buttons=[[
-                         Button.url("FOR PERSONAL USE", url="https://github.com/vasusen-code/videoconvertor/tree/main"),
-                         Button.url("FOR YOUR CHANNEL ", url="https://github.com/vasusen-code/videoconvertor/")]])
+                         Button.url("FOR PERSONAL USE", url="https://github.com/Sharfatali/videoencoder/"),
+                         Button.url("FOR YOUR CHANNEL ", url="https://github.com/Sharfatali/videoencoder/tree/public")]])
                          
                     
 @Drone.on(events.callbackquery.CallbackQuery(data="help"))
