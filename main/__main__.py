e880e67784611f4bbdc0f71a2bb23071fdf14d7c
myck786 <104513778+myck786@users.noreply.github.com>
2022-06-07 09:48:10 +0500

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -16,7 +16,7 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Successfully deployed!")
-print("@a2ztoons_admin • @a2ztoonsindia")
+print("@ahmadofficial • @ahmadofficial")
 
 if __name__ == "__main__":
     Drone.run_until_disconnected()
